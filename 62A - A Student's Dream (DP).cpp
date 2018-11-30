e19d48babfc4cededd41a5367446763455d72577
Mystery Man <42812456+peoplewhoplay@users.noreply.github.com>
2018-11-30 20:32:57 +0700

    Update 62A - A Student's Dream (DP).cpp

--- 62A - A Student's Dream (DP).cpp
+++ 62A - A Student's Dream (DP).cpp
@@ -1,69 +1,55 @@
 #include <iostream>
+#include <omp.h>
+#include <cstdio>
 using namespace std;
 
-int pass[2] = {0,0};    //Initialize result array
+int pass[2] = {0,0};
+int pass2[2] = {0,0};    //Initialize result array
 
 //Function to test whether the two can hold hands comfortably with the given parameters (check main function)
-void test (int f, int m)
+int test (int f, int m)
 {
-    //Initialize modifier for girl's left hand 
-    int t1 = f + 1;   
-    int t2 = f - 1;   
+    //Initialize modifier for girl's left hand
+    int t1 = f + 1;
+    int t2 = f - 1;
     int checker = 0;
-    
+
     //If the number of boy's fingers is equal to or more than the girl's fingers
     if (t2 <= m)
     {
-        pass[checker] = 1;    //Problem solved, set current index of array pass to 1 
-        checker++;            //Problem solved, set current index of array pass to 1 
+        checker++;
     }
-    
+
     //If the number of girl's fingers * 2 exceeds the boy's fingers
     if (m <= 2*t1)
     {
-        pass[checker] = 1;    //Problem solved, set current index of array pass to 1 
-        checker++;            //Problem solved, set current index of array pass to 1 
+        checker++;            //Problem solved, set current index of array pass to 1
     }
 
+    return checker;
 }
 
 //Main function
 int main()
 {
-    int f1, f2;     //Intialize number of girl's fingers
-    int m1, m2;     //Initialize number of boy's fingers
+	int f[2];
+	int m[2];
 
-    int pass1 = 0;
-    int pass2 = 0;
+    cin >> f[0] >> f[1];
+	cin >> m[0] >> m[1];
 
-    cin >> f1 >> f2;      //Input number of girl's fingers
-    cin >> m1 >> m2;      //Input number of boy's fingers
+	pass[0] = max(test(f[0],m[1]),test(f[1],m[0]));         //Test first two problems (girl first)
     
-    //Break the problem into two and solve each one of them
-    for (int a = 0; a < 2; a++)
-    {
-        //Test whether the girl's left hand fits the boy's right hand
-        if (a == 0)
-        {
-            test (f1,m2);
-        }
-        
-        //Test whether the girl's right hand fits the boy's left hand
-        if (a == 1)
-        {
-            test (f2,m1);
-        }
-    }
-  
-    //Following the previous tests, if they are successful as indicated with pass[0] & pass[1] == 1
-    if (pass[0] == 1 && pass[1] == 1)
-    {
-        cout << "YES";      //Print "YES"
-    }
+    pass2[0] = max(test(m[1],f[0]),test(m[0],f[1]));        //Test next two problems (boy first)
     
-    //If all previous tests failed
+    //Get YES/No
+    if (pass[0] == 2 || pass[1] == 2)
+    {
+        cout << "YES";
+    }
+
     else
     {
-        cout << "NO";     //Print "NO"
+        cout << "NO";
     }
 }
